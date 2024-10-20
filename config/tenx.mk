40e25748f182f7cf6b65ca569504353063a17687
Libra420T <ryantf420@gmail.com>
2024-10-20 09:55:43 -0400

    Always Use Blur

--- config/tenx.mk
+++ config/tenx.mk
@@ -28,11 +28,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
     sys.fflag.override.settings_enable_monitor_phantom_procs=false
 
 # Blurs
-ifeq ($(TARGET_ENABLE_BLUR), true)
 PRODUCT_SYSTEM_EXT_PROPERTIES += \
     ro.sf.blurs_are_expensive=1 \
     ro.surface_flinger.supports_background_blur=1 
-endif
 
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.launcher.blur.appLaunch=0
